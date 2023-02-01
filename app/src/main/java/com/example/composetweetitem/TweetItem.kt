a548fd24660f4f20676aaec8c6b463ad275929c8
aluengo <alberto@attomic.es>
2023-02-01 17:31:28 +0100

    finished tweet

--- app/src/main/java/com/example/composetweetitem/TweetItem.kt
+++ app/src/main/java/com/example/composetweetitem/TweetItem.kt
@@ -1,28 +1,26 @@
 package com.example.composetweetitem
 
-import android.app.Activity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontVariation.weight
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,29 +30,35 @@ import com.example.composetweetitem.ui.theme.Background
 @Preview()
 @Composable
 fun TweetItem() {
-    Row(
-        Modifier
-            .fillMaxWidth()
-            .padding(8.dp).background(Background)
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.profile),
-            contentDescription = "Item",
-            modifier = Modifier
-                .size(60.dp)
-                .clip(CircleShape)
-        )
+    Column( Modifier
+        .fillMaxWidth()) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .background(Background)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.profile),
+                contentDescription = "Item",
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape)
+            )
 
-        Column(modifier = Modifier.padding(horizontal = 8.dp)) {
-            TweetInfo()
-            TweetContent()
-            SocialActions()
+            Column(modifier = Modifier.padding(horizontal = 8.dp)) {
+                TweetInfo()
+                TweetContent()
+                SocialActions()
+
+            }
         }
+        com.example.composetweetitem.Divider()
     }
 }
 
 @Composable
-fun TweetContent(){
+fun TweetContent() {
     Text(
         color = Color.White,
         text = "Texto descricicon infernal del averno en tu cara si señor que tal va con un texto mas largo para que salte de linea y tal",
@@ -81,21 +85,13 @@ fun TweetInfo() {
             color = Color.White,
             fontSize = 14.sp,
             // color = Color.White,
-            fontWeight = FontWeight.SemiBold,
+            fontWeight = FontWeight.Bold,
             text = "Aris", modifier = Modifier.padding(0.dp)
         )
-        Text(
-            fontSize = 14.sp,
-            color = Color.Gray,
-            text = "@ArtistiDevs", modifier = Modifier.padding(start = 5.dp)
-        )
-        Text(
-            fontSize = 14.sp,
-            color = Color.Gray,
-            text = "4h", modifier = Modifier
-                .padding(start = 5.dp)
-                .weight(1.0f)
-        )
+        DefaultText("@ArtistiDevs")
+        DefaultText("4h")
+
+        Spacer(modifier = Modifier.weight(1.0f))
         //val activity = LocalContext.current as Activity
         Icon(
             tint = Color.White,
@@ -110,7 +106,24 @@ fun TweetInfo() {
 }
 
 @Composable
+fun DefaultText(text: String) {
+    Text(
+        fontSize = 14.sp,
+        color = Color.Gray,
+        text = text, modifier = Modifier.padding(start = 5.dp)
+    )
+}
+
+@Composable
 fun SocialActions() {
+    var commentState by rememberSaveable { mutableStateOf(false) }
+    var rtState by rememberSaveable { mutableStateOf(false) }
+    var likeState by rememberSaveable { mutableStateOf(false) }
+
+    var commentNumberState by rememberSaveable { mutableStateOf(0) }
+    var rtNumberState by rememberSaveable { mutableStateOf(0) }
+    var likeNumberState by rememberSaveable { mutableStateOf(0) }
+
     Row(
         modifier = Modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
@@ -119,50 +132,78 @@ fun SocialActions() {
         Row(verticalAlignment = Alignment.CenterVertically) {
             Icon(
                 tint = Color.Gray,
-                painter = painterResource(id = R.drawable.ic_chat),
+                painter = painterResource(id = if (commentState) R.drawable.ic_chat_filled else R.drawable.ic_chat),
                 contentDescription = null,
                 modifier = Modifier.clickable {
-//Todo  var state by rememberSaveable { mutableStateOf(true) }
+
+                    if (commentState) {
+                        commentNumberState--
+                    } else {
+                        commentNumberState++
+                    }
+                    commentState = commentState.not()
+
                 })
             Text(
                 fontSize = 13.sp,
                 color = Color.Gray,
-                text = "1",
+                text = commentNumberState.toString(),
                 modifier = Modifier.padding(start = 5.dp)
             )
         }
 
         Row(verticalAlignment = Alignment.CenterVertically) {
             Icon(
-                tint = Color.Gray,
+                tint = if (rtState) Color.Green else Color.Gray,
                 painter = painterResource(id = R.drawable.ic_rt),
                 contentDescription = null,
                 modifier = Modifier.clickable {
-
+                    if (rtState) {
+                        rtNumberState--
+                    } else {
+                        rtNumberState++
+                    }
+                    rtState = rtState.not()
                 })
             Text(
                 fontSize = 13.sp,
                 color = Color.Gray,
-                text = "2",
+                text = rtNumberState.toString(),
                 modifier = Modifier.padding(start = 5.dp)
             )
         }
 
         Row(verticalAlignment = Alignment.CenterVertically) {
             Icon(
-                tint = Color.Gray,
-                painter = painterResource(id = R.drawable.ic_like),
+                tint = if (likeState) Color.Red else Color.Gray,
+                painter = painterResource(id = if (likeState) R.drawable.ic_like_filled else R.drawable.ic_like),
                 contentDescription = null,
                 modifier = Modifier.clickable {
-
+                    if (likeState) {
+                        likeNumberState--
+                    } else {
+                        likeNumberState++
+                    }
+                    likeState = likeState.not()
                 })
             Text(
                 fontSize = 13.sp,
                 color = Color.Gray,
-                text = "3",
+                text = likeNumberState.toString(),
                 modifier = Modifier.padding(start = 5.dp)
             )
         }
 
     }
+}
+
+@Composable
+fun Divider() {
+    Divider(
+        color = Color.Gray,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp)
+            .height(1.dp)
+    )
 }
